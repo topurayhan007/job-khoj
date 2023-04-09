a99ace661b1602f2bba74079e279fc9ae1609c24
topurayhan007 <topurayhan007@gmail.com>
2023-04-10 01:53:18 +0600

    Created Jobs json file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../Banner/Banner";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import JobCategoryContainer from "../JobCategoryContainer/JobCategoryContainer";
 import { useLoaderData } from "react-router-dom";
+import JobsContainer from "../JobsContainer/JobsContainer";
 
 const Home = () => {
   const { categories, jobs } = useLoaderData();
@@ -12,6 +13,8 @@ const Home = () => {
       <Banner></Banner>
       <SectionHeader>Job Category List</SectionHeader>
       <JobCategoryContainer categories={categories}></JobCategoryContainer>
+      <SectionHeader>Featured Jobs</SectionHeader>
+      <JobsContainer jobs={jobs}></JobsContainer>
     </div>
   );
 };
---
+++ src/components/JobsContainer/JobsContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Job from "../Job/Job";
+
+const JobsContainer = ({ jobs }) => {
+  return (
+    <div>
+      <Job></Job>
+    </div>
+  );
+};
+
+export default JobsContainer;
