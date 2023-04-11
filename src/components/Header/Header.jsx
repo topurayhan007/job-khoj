23d216e4ab678765e3404d8ef07db23acf5ed47c
topurayhan007 <topurayhan007@gmail.com>
2023-04-11 15:00:18 +0600

    responsive in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,23 +10,23 @@ const Header = () => {
     <header
       className={location.pathname === "/" ? "bg-[#f9f9ff]" : "bg-transparent"}
     >
-      <nav className="flex justify-between items-center w-10/12 mx-auto pt-10">
+      <nav className="flex md:flex-row flex-col justify-between md:items-center items-start md:text-center text-left md:w-10/12 md:px-0 px-3 mx-auto pt-10 md:gap-0 gap-4">
         <div>
-          <h3 className="text-4xl text-[#1A1919] font-extrabold">
+          <h3 className="md:text-4xl text-3xl text-[#1A1919] font-extrabold">
             Job
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF]">
               Khoj
             </span>
           </h3>
         </div>
-        <div className="flex gap-8 justify-center text-base font-bold">
+        <div className="md:flex md:flex-row hidden flex-col gap-8 justify-center text-base font-bold">
           <ActiveLink to="/">Home</ActiveLink>
           <ActiveLink to="/statistics">Statistics</ActiveLink>
           <ActiveLink to="/appliedjobs">Applied Jobs</ActiveLink>
           <ActiveLink to="/blog">Blog</ActiveLink>
         </div>
         <div>
-          <button className="text-base text-white font-bold bg-gradient-to-r from-[#7E90FE] to-[#9873FF] rounded-lg px-6 py-3">
+          <button className="text-base md:block hidden text-white font-bold bg-gradient-to-r from-[#7E90FE] to-[#9873FF] rounded-lg px-6 py-3">
             Start Applying
           </button>
         </div>
